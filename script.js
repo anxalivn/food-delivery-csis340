e73fdb96817a0fe4731802201f6a9986d78f6483
Vy Dao <daoquangvy2414@gmail.com>
2020-06-14 15:28:38 -0500

    Updated css, script, and information page

---
+++ script.js
@@ -0,0 +1,18 @@
+'user strict';
+
+var AWS = require('aws-sdk'),
+  mydocumentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = function(event, context, callback){
+  var params = {
+    TableName: "UserDatabase"
+  };
+  mydocumentClient.scan(params,function(err,data){
+    if (err) {
+      callback(err,null);
+    }
+    else{
+      callback(null,data.Items);
+    }
+  });
+}
