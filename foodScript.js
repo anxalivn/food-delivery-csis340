78f150d6b8703d96aaa6a89b77ee7091df5b45be
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2020-06-15 19:41:53 -0500

    Update foodScript.js

--- foodScript.js
+++ foodScript.js
@@ -2,9 +2,25 @@
 
 const api_foods_url = 'https://sfxz3aprr7.execute-api.us-east-1.amazonaws.com/Finish1/getmenuinformation';
 async function getData() {
-  const response = await fetch (api_foods_url, {mode: 'cors'});
-  const foods = await response.json();
-  console.log(foods);
+    const response = await fetch(api_foods_url, { mode: 'cors' });
+    const foods = await response.json();
+    console.log(foods);
 }
 
 getData();
+
+buildTable(foods);
+
+function buildTable(foods) {
+    var table = document.getElementById('foodMenu')
+
+    for (var i = 0; i < data.length; i++) {
+        var row = ' <tr>    
+            < td > ${foods[i].foodName }</td>
+            < td > ${foods[i].foodDescription}</td>
+            < td > ${foods[i].foodPrice}</td>
+                </tr > '
+        table.innerHTML += row 
+    }
+
+}
