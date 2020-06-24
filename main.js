6aa41bc5a8713735187218639d61267cc66fa79c
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2020-06-24 11:02:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
             foodData += '<td>' + value.foodDescription + '</td>';
             foodData += '<td>' + value.foodPrice + '</td>';
             foodData += '< td > <img src= "Images/ ' + value.foodImage.toString() + ' "> ' + ' alt="" border=3 height = 100 width = 100 /> </td > ';
-            foodData += '<td>  < button class="bag-btn" data - id="1" > <i class="add-cart cart' + i + '></i>  add to bag </button > </td >';
+            foodData += '<td>  < button class="bag-btn" data - id="1" > <i class="add-cart"></i>  add to bag </button > </td >';
             foodData += '</tr>';
             i += 1;
         });
