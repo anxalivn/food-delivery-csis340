2d8a94db9b519657716b48b4fa83e6cb4dbf0355
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2020-06-24 11:57:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
 let carts = document.querySelectorAll('.add-cart');
 
-'user strict';
-
-const api_foods_url = 'https://sfxz3aprr7.execute-api.us-east-1.amazonaws.com/Finish1/getmenuinformation';
-async function getData() {
-    const response = await fetch(api_foods_url, { mode: 'cors' });
-    const foods = await response.json();
-    console.log(foods);
-}
-
-getData();
-
-$(document).ready(function () {
-    $.getJSON(api_foods_url, function (foods) {
-        var foodData = '';
-        $.each(foods, function (key, value) {
-            
-            foodData += '<tr>';
-            foodData += '<td>' + value.foodName + '</td>';
-            foodData += '<td>' + value.foodDescription + '</td>';
-            foodData += '<td>' + value.foodPrice + '</td>';
-            foodData += '< td > <img src= "Images/ ' + value.foodImage.toString() + ' "> ' + ' alt="" border=3 height = 100 width = 100 /> </td > ';
-            foodData += '<td> <a class="add-cart cart1" href="#">Add Cart</a> </td >';
-        });
-        $('#foodMenu').append(foodData);
-    });
-});
 
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
