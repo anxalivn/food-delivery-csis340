94e598a821ba006890a47cfe6a91dc6b8dd1d127
Khangal Ganbaatar <56380294+anxalivn@users.noreply.github.com>
2020-06-30 10:46:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,27 @@ $(document).ready(function () {
     })
 })
 
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("foodMenu");
+  tr = table.getElementsByTagName("tr");
 
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 function addFood(food){
     jsons.push(food);
     console.log(jsons)
